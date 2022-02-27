6e7c2102a2bfa82cdf84a4d416bf6d493bf9f6bc
Adam Kakes <adamkakes@gmail.com>
2022-02-27 20:53:38 +0100

    Přidejte soubory projektu.

---
+++ CifernySoucet.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int soucet = 0;
+    int cislo = 12345;
+    while (cislo)
+    {
+        soucet += cislo % 10;
+        cislo /= 10;
+    }
+    printf("Ciferny soucet = %d\n", soucet);
+}
